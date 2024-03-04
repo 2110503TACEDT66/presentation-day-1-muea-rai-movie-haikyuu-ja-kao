8b768893730de5804003a94cc619405a18cf6c4a
ravisaradx <145831809+ravisaradx@users.noreply.github.com>
2024-03-04 18:54:44 +0700

    edit createReservation  res.status

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -101,7 +101,7 @@ exports.addReservation = async (req, res, next) => {
         }
 
         const reservation = await Reservation.create(req.body);
-        res.status(200).json({success: true, data: reservation});
+        res.status(201).json({success: true, data: reservation});
     } catch (err) {
         console.log(error.stack);
         return res.status(500).json({success: false, message: "Cannot create reservation"});
