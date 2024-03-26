8e5f15c49226536c30efeadcf0d9b7e48a13057e
ravisaradx <145831809+ravisaradx@users.noreply.github.com>
2024-03-26 11:09:09 +0700

    update config.env, server.js

--- server.js
+++ server.js
@@ -31,8 +31,30 @@ app.use('/api/v1/coWorkingSpaces',coWorkingSpaces);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/reservations', reservations);
 
-const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ', PORT));
+const PORT=process.env.PORT || 5001;
+const server = app.listen(
+    PORT, 
+    console.log('Server running in ',process.env.NODE_ENV,
+    "on" + process.env.HOST + ":" + PORT));
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openai: '3.0.0',
+        info:{
+            title: 'Library API',
+            version: '1.0.0',
+            description: 'Co-working Space API'
+        },
+        servers: [
+            {
+                url: process.env.HOST + ':' + PORT + 'api/v1'
+            }
+        ],
+    },
+    apis:['./route/*.js'],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise)=>{
